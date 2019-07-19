78649ea24ac008d2919a01b9dadbe30874c0e7b5
Mitch Andrews <rastaninja77@gmail.com>
2019-07-19 00:27:33 -0400

    😎 Second moving block

--- Game/Source/Cores/APCore.h
+++ Game/Source/Cores/APCore.h
@@ -5,6 +5,8 @@
 #include "Engine/World.h"
 #include "ECS/Component.h"
 #include "Components/Graphics/Model.h"
+#include "Engine/Input.h"
+#include "Math/Vector3.h"
 
 class StackBlock final
 	: public Component<StackBlock>
@@ -119,6 +121,11 @@ public:
 			}
 		}
 		transform.SetPosition(m_currentPosition);
+
+		if (Input::GetInstance().IsKeyDown(KeyCode::Space))
+		{
+
+		}
 	}
 
 private:
@@ -128,18 +135,31 @@ private:
 
 	virtual void OnStart() override
 	{
+		m_previousBlock = GetWorld().CreateEntity().lock();
+		Transform& prevTransform = m_previousBlock->AddComponent<Transform>("Root StackBlock");
+		Vector3 prevScale = prevTransform.GetScale();
+		prevScale.SetX(2.f);
+		prevScale.SetY(2.f);
+		prevScale.SetZ(2.f);
+		prevTransform.SetScale(prevScale);
+		StackBlock& prevBlock = m_previousBlock->AddComponent<StackBlock>();
+		prevBlock.BlockMoveSpeed = 0.f;
+		m_previousBlock->AddComponent<Model>("Assets/Cube.fbx");
+
 		m_currentBlock = GetWorld().CreateEntity().lock();
 		Transform& transform = m_currentBlock->AddComponent<Transform>("Root StackBlock");
+		transform.SetPosition(prevTransform.Position + Vector3(0.f, (prevTransform.GetScale().Y()) + (.3f), 0.f));
 		Vector3 scale = transform.GetScale();
 		scale.SetX(2.f);
-		scale.SetY(0.2f);
+		scale.SetY(.3f);
 		scale.SetZ(2.f);
 		transform.SetScale(scale);
-		m_currentBlock->AddComponent<StackBlock>();
+		StackBlock& block = m_currentBlock->AddComponent<StackBlock>();
 		m_currentBlock->AddComponent<Model>("Assets/Cube.fbx");
 	}
 
 	SharedPtr<Entity> m_currentBlock;
+	SharedPtr<Entity> m_previousBlock;
 	Vector3 m_currentPosition;
 };
 ME_REGISTER_CORE(APCore)
